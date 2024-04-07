9a1bb33dfc4c77a2360bfa534e6b58cb4caad1d5
ARBUCHELI <radio_seattle@hotmail.com>
2024-04-07 17:34:08 +0300

    functions - defining functions

---
+++ FUNCTIONS/defining-functions/script.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Write your code below
+void printMyFavoriteAnimal()
+{
+  printf("My favorite animal is a Cat called Soso\n");
+}
+
+int main(void) {
+  printMyFavoriteAnimal();
+}
