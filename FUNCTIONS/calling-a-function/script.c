9cd19f2640ceaa1a8b39f39499ec0019e28feb8c
ARBUCHELI <radio_seattle@hotmail.com>
2024-04-07 17:00:52 +0300

    functions - calling a function

---
+++ FUNCTIONS/calling-a-function/script.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  int num1 = -5;
+  int num2 = 42;
+  // Write your code below
+  //printf("My name is Bucheli");
+  printf("This is a negative number: %i, and this is a positive number: %i", num1, num2);
+}
